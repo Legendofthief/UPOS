98e1c32b657a1399f8baa0756a17880ca582693d
Vladimir <vladimir.mescherjakov@gmail.com>
2022-10-05 22:36:32 +0300

    Calc4Life

---
+++ Calc4Life/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calc4Life
+{
+    class Calculator
+    {
+        static private bool Split(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+        static private bool FindOperator(char с)
+        {
+            if (("+-/*^()".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+        static private byte Priority(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 2;
+                case '*': return 3;
+                case '/': return 3;
+                default: return 4;
+            }
+        }
+        static public double Rezult(string input)
+        {
+            string output = Getinstance(input);
+            double result = Calculate(output);
+            return result;
+        }
+        static private string Getinstance(string input)
+        {
+            string output = string.Empty;
+            Stack<char> operStack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (Split(input[i]))
+                    continue;
+
+                if (Char.IsDigit(input[i]))
+                {
+
+                    while (!(Split(input[i]) || FindOperator(input[i])))
+                    {
+                        output += input[i];
+                        i++;
+
+                        if (i == input.Length) break;
+                    }
+
+                    output += " ";
+                    i--;
+                }
+
+                if (FindOperator(input[i]))
+                {
+                    if (input[i] == '(')
+                        operStack.Push(input[i]);
+                    else if (input[i] == ')')
+                    {
+                        char s = operStack.Pop();
+
+                        while (s != '(')
+                        {
+                            output += s.ToString() + ' ';
+                            s = operStack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        if (operStack.Count > 0)
+                            if (Priority(input[i]) <= Priority(operStack.Peek()))
+                                output += operStack.Pop().ToString() + " ";
+
+                        operStack.Push(char.Parse(input[i].ToString()));
+                    }
+                }
+            }
+
+
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+
+            return output;
+        }
+        static private double Calculate(string input)
+        {
+            double result = 0;
+            Stack<double> temp = new Stack<double>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    string a = string.Empty;
+
+                    while (!(Split(input[i]) && !FindOperator(input[i])))
+                    {
+                        a += input[i];
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    temp.Push(double.Parse(a));
+                    i--;
+                }
+                else if (FindOperator(input[i]))
+                {
+                    double a = temp.Pop();
+                    double b = temp.Pop();
+
+                    switch (input[i])
+                    {
+                        case '+': result = b + a; break;
+                        case '-': result = b - a; break;
+                        case '*': result = b * a; break;
+                        case '/': result = b / a; break;
+                    }
+                    temp.Push(result);
+                }
+            }
+            return temp.Peek();
+        }
+    }
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Выражение: ");
+                Console.WriteLine(Calculator.Rezult(Console.ReadLine()));
+            }
+        }
+    }
+}
