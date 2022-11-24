c533dfb6df973b06f48c99e245f8ebd58f95c190
Vladimir <vladimir.mescherjakov@gmail.com>
2022-11-24 21:35:08 +0300

    New Calc

---
+++ Calculator(final touch)/ProjectConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using Calculator;
+using System;
+
+class programm
+{
+    static void Main(string[] args)
+    {
+        bool exit = false;
+        TParser parser = new TParser();
+        double result;
+        while (exit == false)
+        {
+            Console.Write("Введите выражение (exit для выхода):");
+            string expression = Console.ReadLine();
+            if (expression == "exit") break;
+
+            result=parser.eval_exp(expression);
+            Console.WriteLine("Ответ:"+result.ToString());
+        }
+    }
+}
